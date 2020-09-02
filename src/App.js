92b8774461c526c4241bbc7d18587f743df5be7b
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-09-02 03:20:02 +0100

    ready

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useReducer } from "react";
 import { API } from "aws-amplify";
-import { LIST } from "antd";
+import { List } from "antd";
 import "antd/dist/antd.css";
 import { listNotes } from "./graphql/queries";
 
@@ -12,6 +12,12 @@ const initialState = {
     error: false,
     form: { name: "", description: "" },
 };
+const styles = {
+    container: { padding: 20 },
+    input: { marginBottom: 10 },
+    item: { textAlign: "left" },
+    p: { color: "#1890ff" },
+};
 function reducer(state, action) {
     switch (action.type) {
         case "SET_NOTES":
@@ -22,7 +28,21 @@ function reducer(state, action) {
             return state;
     }
 }
+
 export default function App() {
+    useEffect(() => {
+        fetchNotes();
+    }, []);
+    function renderItem(item) {
+        return (
+            <List.Item style={styles.item}>
+                <List.Item.Meta
+                    title={item.name}
+                    description={item.description}
+                />
+            </List.Item>
+        );
+    }
     const [state, dispatch] = useReducer(reducer, initialState);
     async function fetchNotes() {
         try {
@@ -38,4 +58,13 @@ export default function App() {
             dispatch({ type: "ERROR" });
         }
     }
+    return (
+        <div style={styles.container}>
+            <List
+                loading={state.loading}
+                dataSource={state.notes}
+                renderItem={renderItem}
+            />
+        </div>
+    );
 }
