c53ab1c6ca63ca5249c694125f64464e4f98fa67
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-09-02 02:10:52 +0100

    initial state

--- src/App.js
+++ src/App.js
@@ -1,41 +1,24 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import React from "react";
+import React, { useEffect, useReducer } from "react";
+import { API } from "aws-amplify";
+import { LIST } from "antd";
+import "antd/dist/antd.css";
+import { listNotes } from "./graphql/queries";
 
 import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 
+const initialState = {
+    notes: [],
+    loading: true,
+    error: false,
+    form: { name: "", description: "" },
+};
 function App() {
-  return (
-    <div>
-      <h1>Hello from AWS Amplify</h1>
-      <AmplifySignOut />
-    </div>
-  );
+    return (
+        <div>
+            <h1>Hello from AWS Amplify</h1>
+            <AmplifySignOut />
+        </div>
+    );
 }
 
 export default withAuthenticator(App);
